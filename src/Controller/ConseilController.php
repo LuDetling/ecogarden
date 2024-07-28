736d6fc2da5d8409c076f9b689d5a1c77df3064d
Lucas <lucas.detling@gmail.com>
2024-07-28 02:52:57 +0200

    meteo city

--- src/Controller/ConseilController.php
+++ src/Controller/ConseilController.php
@@ -6,37 +6,16 @@ use App\Repository\ConseilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ConseilController extends AbstractController
 {
 
     public function __construct(
-        private HttpClientInterface $client,
         private ConseilRepository $conseilRepository,
     ) {
     }
 
-    public function fetchWeatherApi(int $zip): array
-    {
 
-        //cacher app id dans env
-        $latLong = $this->client->request(
-            'GET',
-            'https://api.openweathermap.org/geo/1.0/zip?zip=' . $zip . ',FR&appid=' . $this->getParameter('app.weather_api')
-        );
-
-        $content = $latLong->toArray();
-        $lat = $content["lat"];
-        $lon = $content["lon"];
-
-        $response = $this->client->request(
-            'GET',
-            'https://api.openweathermap.org/data/2.5/weather?lat=' . $lat . '&lon=' . $lon . '&appid=' . $this->getParameter('app.weather_api')
-        );
-
-        return $response->toArray();
-    }
 
     #[Route('/conseil', name: 'app_conseil')]
     public function conseils(): Response
@@ -45,6 +24,18 @@ class ConseilController extends AbstractController
         // // a changer le zipcode
         // $this->fetchWeatherApi(37100);
 
+        $conseil = $this->conseilRepository->findBy([
+            'month' => $month
+        ]);
+
+        return $this->json(data: [
+            'conseil' => $conseil,
+        ], context: ['groups' => ['list_conseil']]);
+    }
+
+    #[Route('/conseil/{month}', name: 'app_conseil_month')]
+    public function conseil(int $month): Response
+    {
         $conseil = $this->conseilRepository->findBy([
             'month' => $month
         ]);
---
+++ src/Controller/MeteoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MeteoController extends AbstractController
+{
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
+    public function fetchWeatherApi(string $city): array
+    {
+
+        $response = $this->client->request(
+            'GET',
+            'https://api.openweathermap.org/data/2.5/weather?q=' . $city . '&appid=' . $this->getParameter('app.weather_api')
+        );
+
+        if ($response->getStatusCode() === 404) return [
+            'error' => 'Ville non trouvée'
+        ];
+
+        return $response->toArray();
+    }
+
+    #[Route('/meteo/{city}', name: 'app_meteo_city')]
+    public function meteoCity(string $city): Response
+    {
+
+        $meteo = $this->fetchWeatherApi($city);
+
+        return $this->json(data: [
+            'meteo' => $meteo,
+        ]);
+    }
+    #[Route('/meteo', name: 'app_meteo')]
+    public function meteo(): Response
+    {   
+        // a changer
+        $user = $this->getUser();
+        dd($user);
+        $city = "";
+        $meteo = $this->fetchWeatherApi($city);
+
+        return $this->json(data: [
+            'meteo' => $meteo,
+        ]);
+    }
+}
